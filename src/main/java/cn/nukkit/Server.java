787ee43fdd53ef0363777b5cfe73f3b4a8f2a837
Petteri <petteri.malkavaara1@gmail.com>
2018-01-05 16:25:35 +0200

    Update Server.java

--- src/main/java/cn/nukkit/Server.java
+++ src/main/java/cn/nukkit/Server.java
@@ -645,7 +645,7 @@ public class Server {
             return true;
         }
 
-        sender.sendMessage(new TranslationContainer(TextFormat.RED + "%commands.generic.unknown", commandLine));
+        sender.sendMessage(new TranslationContainer(TextFormat.RED + "%commands.generic.notFound", commandLine));
 
         return false;
     }
