4beb82fbd0ee8c8060cb0c27b4592852513724b5
Petteri <petteri.malkavaara1@gmail.com>
2018-01-06 13:27:45 +0200

    UpdateAttributesPacket

--- src/main/java/cn/nukkit/network/Network.java
+++ src/main/java/cn/nukkit/network/Network.java
@@ -293,6 +293,7 @@ public class Network {
         this.registerPacket(ProtocolInfo.TAKE_ITEM_ENTITY_PACKET, TakeItemEntityPacket.class);
         this.registerPacket(ProtocolInfo.TEXT_PACKET, TextPacket.class);
         this.registerPacket(ProtocolInfo.TRANSFER_PACKET, TransferPacket.class);
+        this.registerPacket(ProtocolInfo.UPDATE_ATTRIBUTES_PACKET, UpdateAttributesPacket.class);
         this.registerPacket(ProtocolInfo.UPDATE_BLOCK_PACKET, UpdateBlockPacket.class);
         this.registerPacket(ProtocolInfo.UPDATE_TRADE_PACKET, UpdateTradePacket.class);
     }
