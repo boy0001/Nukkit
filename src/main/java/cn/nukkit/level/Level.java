50b6cc1d16e7af8e7ac541f97f75d92f04968916
PetteriM1 <petteri.malkavaara1@gmail.com>
2018-01-22 20:05:09 +0200

    Revert "..."
    This reverts commit 2b9dd86012e94c876ceeccffd4e294d8972e7378.

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -1296,9 +1296,6 @@ public class Level implements ChunkManager, Metadatable {
         return list;
     }
 
-    public Set<BlockUpdateEntry> getPendingBlockUpdates(AxisAlignedBB boundingBox) {
-        return updateQueue.getPendingBlockUpdates(boundingBox);
-
     public Block[] getCollisionBlocks(AxisAlignedBB bb) {
         return this.getCollisionBlocks(bb, false);
     }
